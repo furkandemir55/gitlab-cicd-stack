b5a23b320e0a6ffa78d0eb27900cd5c2485f40fc
Furkan Demir <furkan.demir@dt.net.tr>
2022-03-30 16:47:18 +0300

    env.example

--- setup-project.sh
+++ setup-project.sh
@@ -11,23 +11,29 @@ read BRANCH_NAME
 printf "RUNNER TAG(default=arge-server): "
 read RUNNER_TAG
 
-sed -i "s/PROJECT_NAME=.*/PROJECT_NAME=$PROJECT_NAME/" .env
-if [ "$PROJECT_PATH" ]; then
-  sed -i "s/PROJECT_PATH=.*/PROJECT_PATH=$PROJECT_PATH/" .env
+cp env.example .env
+
+if [ -z "$PROJECT_PATH" ]; then
+  PROJECT_PATH=/arge
 fi
-if [ "$VOLUME_PATH" ]; then
-  sed -i "s/VOLUME_PATH=.*/VOLUME_PATH=$VOLUME_PATH/" .env
+if [ -z "$VOLUME_PATH" ]; then
+  VOLUME_PATH=/volume
 fi
-if [ "$BRANCH_NAME" ]; then
-  sed -i "s/BRANCH_NAME=.*/BRANCH_NAME=$BRANCH_NAME/" .env
+if [ -z "$BRANCH_NAME" ]; then
+  BRANCH_NAME=master
 fi
-if [ "$RUNNER_TAG" ]; then
-  sed -i "s/RUNNER_TAG=.*/RUNNER_TAG=$RUNNER_TAG/" .env
+if [ -z "$RUNNER_TAG" ]; then
+  RUNNER_TAG=arge-server
 fi
 
+POSTGRES_USER="user_$(openssl rand -hex 6)"
+POSTGRES_PASSWORD=$(openssl rand -hex 16)
+sed -i "s/POSTGRES_USER=.*/POSTGRES_USER=$POSTGRES_USER/" .env
+sed -i "s/POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=$POSTGRES_PASSWORD/" .env
 
-#POSTGRES_USER="user_$(openssl rand -hex 6)"
-#sed -i "s/POSTGRES_USER=.*/POSTGRES_USER=$POSTGRES_USER/" .env
-#
-#POSTGRES_PASSWORD=$(openssl rand -hex 16)
-#sed -i "s/POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=$POSTGRES_PASSWORD/" .env
+sed -i "s;PROJECT_PATH=.*;PROJECT_PATH=$PROJECT_PATH;" .env
+sed -i "s;VOLUME_PATH=.*;VOLUME_PATH=$VOLUME_PATH;" .env
+sed -i "s;BRANCH_NAME=.*;BRANCH_NAME=$BRANCH_NAME;" .env
+sed -i "s;RUNNER_TAG=.*;RUNNER_TAG=$RUNNER_TAG;" .env
+sed -i "s;PROJECT_NAME=.*;PROJECT_NAME=$PROJECT_NAME;" .env
+
