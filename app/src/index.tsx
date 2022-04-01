c74ff72ae2d7be2b4cab6da04d75cf32a10aeee9
Furkan Demir <furkan.demir@dt.net.tr>
2022-04-01 11:47:27 +0300

    react 18

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,6 +3,6 @@ import App from './App';
 
 import {createRoot} from 'react-dom/client';
 
-const container = document.getElementById('app')!;
+const container = document.getElementById('root')!;
 const root = createRoot(container);
 root.render(<App/>);
