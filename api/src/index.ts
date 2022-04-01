115c4f94c2ddd707a5193d29b530e9d3ff0f26a6
Furkan Demir <furkan.demir@dt.net.tr>
2022-04-01 11:48:09 +0300

    api routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import Cors from 'cors'
 
+import exampleRouter from './routes/example'
+
 const app = express();
 const origin = () => {
     if (process.env.PROJECT_ENV === 'docker') return process.env.HOST_URL
@@ -10,14 +12,12 @@ const cors = {origin: origin(), credentials: true}
 app.set('trust proxy', true)
 app.use(Cors(cors))
 
+app.use('/', exampleRouter)
+
 
 app.get('/', (req, res) => {
     res.json({success: true})
 })
-app.post('/home', (req, res) => {
-    res.json({success: true})
-})
-
 
 const port = process.env.PROJECT_ENV === 'docker' ? 80 : 5000
 app.listen(port, () => console.log(`API is listening at port ${port}`))
