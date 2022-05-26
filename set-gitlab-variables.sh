475e9a45df16a4800ef60884572cec4a76501fad
Furkan Demir <furkan.demir@dt.net.tr>
2022-05-26 13:48:30 +0300

    now reads from .env automatically

--- set-gitlab-variables.sh
+++ set-gitlab-variables.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-#
-#echo "" >>.env
-#
-###add your gitlab variable names here
-#for i in "NGINX_PORT"; do
-#  eval "echo $i=\$$i" >>.env
-#done
 
-#set nginx port
-sed -Ei "s/(NGINX_PORT=).*/\1$NGINX_PORT/" .env
+args=()
+while read -r line; do
+  args+=($(echo "$line" | sed -n 's/\(.*\)=.*/\1/p'))
+done <.env
+
+for i in "${args[@]}"; do
+  if [ -z "${!i}" ]; then
+    echo "$i is not set"
+    exit 1
+  else
+    sed -Ei "s;(${i}=).*;\1${!i};" .env
+  fi
+done
